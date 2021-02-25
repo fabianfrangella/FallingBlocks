b6d53449ac51a550ed9989b8de7dc846ddae68db
Fabian Frangella <fabianfrangella@gmail.com>
2021-02-25 10:23:20 -0300

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuScreen.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mainMenuScreen.SetActive(false);
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
