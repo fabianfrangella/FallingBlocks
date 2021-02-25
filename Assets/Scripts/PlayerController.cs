4e2bfaf43551bcc9c0b1d7d37e6948300f3a945a
Fabian Frangella <fabianfrangella@gmail.com>
2021-02-25 15:35:24 -0300

    movimiento hacía arriba

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,36 @@ public class PlayerController : MonoBehaviour
     public event System.Action OnPlayerDeath;
     public float speed = 7;
     float screenHalfWidthInWorldUnits;
+    float screenHalfHeigthInWorldUnits;
     // Start is called before the first frame update
     void Start()
     {
         screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+        screenHalfHeigthInWorldUnits = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float velocity = inputX * speed;
-        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        MovePlayer();
         CheckBorderCollision();
+    }
+
+    private void MovePlayer()
+    {
+        float velocity = 0;
+        float inputY = Input.GetAxisRaw("Vertical");
+        float inputX = Input.GetAxisRaw("Horizontal");
+        if (inputX != 0)
+        {
+            velocity = inputX * speed;
+            transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        }
+        if (inputY != 0)
+        {
+            velocity = inputY * speed;
+            transform.Translate(Vector2.up * velocity * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D triggerCollider)
@@ -43,6 +60,15 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > screenHalfWidthInWorldUnits + (transform.localScale.x / 2f))
         {
             transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+
+        if (transform.position.y < -(screenHalfHeigthInWorldUnits + (transform.localScale.y / 2f)))
+        {
+            transform.position = new Vector2(transform.position.x, screenHalfHeigthInWorldUnits);
+        }
+        if (transform.position.y > screenHalfHeigthInWorldUnits + (transform.localScale.y / 2f))
+        {
+            transform.position = new Vector2(transform.position.x, -screenHalfHeigthInWorldUnits);
         }
     }
 }
